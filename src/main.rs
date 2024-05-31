ad871ff69ab10c6b4340533b5ef6f99914fce344
Tim Potter <tpot@frungy.org>
2024-05-31 10:26:49 +1000

    Open /dev/kvm device

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::os::fd::{AsRawFd, FromRawFd, OwnedFd};
+use nix::{
+    fcntl, 
+    fcntl::OFlag,
+    sys::stat::Mode,
+};
+
+const KVM_DEVICE: &str = "/dev/kvm";
+
 fn main() {
-    println!("Hello, world!");
+
+    // Open /dev/kvm
+    let kvm_fd: OwnedFd = match fcntl::open(KVM_DEVICE, OFlag::O_RDWR, Mode::empty()) {
+        Ok(fd) => unsafe {
+            assert!(fd != -1);
+            FromRawFd::from_raw_fd(fd)
+        },
+        Err(errno) => {
+            eprintln!("Error opening {KVM_DEVICE}: {errno}");
+            std::process::exit(1);
+        },
+    };
+
+    println!("kvm_fd = {0}", AsRawFd::as_raw_fd(&kvm_fd));
 }
