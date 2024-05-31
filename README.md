3756205dc7ea16b1d16893663334ffa26b5873fc
Tim Potter <tpot@frungy.org>
2024-05-31 11:08:10 +1000

    Fetch and check KVM API version

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Port of
 [kvm-hello-world.c](https://github.com/dpw/kvm-hello-world/blob/master/kvm-hello-world.c)
 to Rust.
+
+## Resources
+
+* Crates
+  * [kvm-bindings] - automatically generated FFI bindings for the KVM API from `linux/kvm.h` using [bindgen](https://crates.io/crates/bindgen)
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use std::os::fd::{AsRawFd, FromRawFd, OwnedFd};
+
 use nix::{
     fcntl, 
     fcntl::OFlag,
     sys::stat::Mode,
+    ioctl_none_bad,
+    request_code_none,
+};
+
+use kvm_bindings::{
+    KVMIO,
+    KVM_API_VERSION,
 };
 
 const KVM_DEVICE: &str = "/dev/kvm";
+
+// Unfortunately the kvm_bindings crate does not export ioctl sequence numbers
+// so we must hardcode them and use "bad" ioctls.
+ioctl_none_bad!(kvm_get_api_version, request_code_none!(KVMIO, 0x00));
 
 fn main() {
 
@@ -22,4 +34,20 @@ fn main() {
     };
 
     println!("kvm_fd = {0}", AsRawFd::as_raw_fd(&kvm_fd));
+
+    // Get KVM API version
+    let api_ver = match unsafe {
+        kvm_get_api_version(kvm_fd.as_raw_fd())
+    } {
+        Ok(result) => {
+            assert!(result == KVM_API_VERSION as i32, "Unknown KVM API version: {result}");
+            result
+        },
+        Err(errno) => {
+            eprintln!("Error in kvm_create_vm: {errno}");
+            std::process::exit(1);
+        },
+    };
+
+    println!("KVM API version = {api_ver}");
 }
