687601ef5ea3e67e1ac66bccd7fcd6e082cb1ab8
Tim Potter <tpot@frungy.org>
2024-06-03 11:42:43 +1000

    Move VM and memory slot init into vm_init() function

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ use kvm_bindings::{
 };
 
 const KVM_DEVICE: &str = "/dev/kvm";
+const MAP_SIZE: usize = 0x1000;
 
 // Unfortunately the kvm_bindings crate does not export ioctl sequence numbers
 // so we must hardcode them and use "bad" ioctls.
@@ -41,96 +42,101 @@ ioctl_none_bad!(kvm_get_vcpu_mmap_size, request_code_none!(KVMIO, 0x04));
 
 ioctl_write_ptr!(kvm_set_user_memory_region, KVMIO, 0x46, kvm_userspace_memory_region);
 
+struct Vm {
+    sys_fd: OwnedFd,
+    vm_fd: OwnedFd,
+    mem: u64,
+}
+
+impl Vm {
+
+    // Create new VM
+    pub fn new() -> Result<Self, nix::Error> {
+
+        // Open /dev/kvm
+        let sys_fd: OwnedFd = match fcntl::open(KVM_DEVICE, OFlag::O_RDWR, Mode::empty()) {
+            Ok(fd) => unsafe {
+                assert!(fd != -1);
+                FromRawFd::from_raw_fd(fd)
+            },
+            Err(errno) => return Err(errno),
+        };
+
+        println!("sys_fd = {0}", AsRawFd::as_raw_fd(&sys_fd));
+
+        // Get KVM API version
+        let api_ver = match unsafe {
+            kvm_get_api_version(AsRawFd::as_raw_fd(&sys_fd))
+        } {
+            Ok(result) => {
+                assert!(result == KVM_API_VERSION as i32, "Unknown KVM API version: {result}");
+                result
+            },
+            Err(errno) => return Err(errno),
+        };
+
+        println!("KVM API version = {api_ver}");
+
+        // Create a VM
+        let vm_fd: OwnedFd = match unsafe {
+            kvm_create_vm(AsRawFd::as_raw_fd(&sys_fd))
+        } {
+            Ok(fd) => unsafe {
+                assert!(fd != -1);
+                FromRawFd::from_raw_fd(fd)
+            },
+            Err(errno) => return Err(errno),
+        };
+
+        println!("vm_fd = {0}", AsRawFd::as_raw_fd(&vm_fd));
+
+        // Create and attach memory
+        let mem = match unsafe {
+            mman::mmap_anonymous(
+                None,
+                NonZeroUsize::new(MAP_SIZE).expect("User memory size is zero"),
+                ProtFlags::PROT_READ | ProtFlags::PROT_WRITE,
+                MapFlags::MAP_ANONYMOUS | MapFlags::MAP_SHARED,
+            )
+        } {
+            Ok(result) => {
+                result.as_ptr() as u64
+            },
+            Err(errno) => return Err(errno),
+        };
+
+        match unsafe {
+            kvm_set_user_memory_region(
+                AsRawFd::as_raw_fd(&vm_fd),
+                &kvm_userspace_memory_region {
+                    slot: 0,
+                    flags: 0,
+                    guest_phys_addr: 0,
+                    memory_size: MAP_SIZE as u64,
+                    userspace_addr: mem,
+                },
+            )
+        } {
+            Ok(_) => {},
+            Err(errno) => return Err(errno),
+        };
+
+        Ok(Self{
+            sys_fd,
+            vm_fd,
+            mem,
+        })
+    }
+}
+
 fn main() {
 
-    // Open /dev/kvm
-    let kvm_fd: OwnedFd = match fcntl::open(KVM_DEVICE, OFlag::O_RDWR, Mode::empty()) {
-        Ok(fd) => unsafe {
-            assert!(fd != -1);
-            FromRawFd::from_raw_fd(fd)
-        },
-        Err(errno) => {
-            eprintln!("Error opening {KVM_DEVICE}: {errno}");
-            std::process::exit(1);
-        },
-    };
-
-    println!("kvm_fd = {0}", AsRawFd::as_raw_fd(&kvm_fd));
-
-    // Get KVM API version
-    let api_ver = match unsafe {
-        kvm_get_api_version(kvm_fd.as_raw_fd())
-    } {
-        Ok(result) => {
-            assert!(result == KVM_API_VERSION as i32, "Unknown KVM API version: {result}");
-            result
-        },
-        Err(errno) => {
-            eprintln!("Error in kvm_create_vm: {errno}");
-            std::process::exit(1);
-        },
-    };
-
-    println!("KVM API version = {api_ver}");
-
-    // Create a VM
-    let kvm_vm_fd: OwnedFd = match unsafe {
-        kvm_create_vm(kvm_fd.as_raw_fd())
-    } {
-        Ok(fd) => unsafe {
-            assert!(fd != -1);
-            FromRawFd::from_raw_fd(fd)
-        },
-        Err(errno) => {
-            eprintln!("Error creating VM: {errno}");
-            std::process::exit(1);
-        },
-    };
-
-    println!("kvm_vm_fd = {0}", AsRawFd::as_raw_fd(&kvm_vm_fd));
-
-    // Set user memory region
-    const MAP_SIZE: usize = 0x1000;
-
-    let user_memory = match unsafe {
-        mman::mmap_anonymous(
-            None,
-            NonZeroUsize::new(MAP_SIZE).expect("User memory size is zero"),
-            ProtFlags::PROT_READ | ProtFlags::PROT_WRITE,
-            MapFlags::MAP_ANONYMOUS | MapFlags::MAP_SHARED,
-        )
-    } {
-        Ok(result) => {
-            result.as_ptr() as u64
-        },
-        Err(errno) => {
-            eprintln!("Error calling mmap(): {errno}");
-            std::process::exit(1);
-        },
-    };
-
-    match unsafe {
-        kvm_set_user_memory_region(
-            AsRawFd::as_raw_fd(&kvm_vm_fd),
-            &kvm_userspace_memory_region {
-                slot: 0,
-                flags: 0,
-                guest_phys_addr: 0,
-                memory_size: MAP_SIZE as u64,
-                userspace_addr: user_memory,
-            },
-        )
-    } {
-        Ok(_) => { }
-        Err(errno) => {
-            eprintln!("Error calling mmap(): {errno}");
-            std::process::exit(1);
-        },
-    };
+    // Initialise VM
+    let vm = Vm::new().expect("Unable to initialise VM");
 
     // Create a vCPU for the VM
     let kvm_vcpu_fd: OwnedFd = match unsafe {
-        kvm_create_vcpu(kvm_vm_fd.as_raw_fd())
+        kvm_create_vcpu(AsRawFd::as_raw_fd(&vm.vm_fd))
     } {
         Ok(fd) => unsafe {
             assert!(fd != -1);
@@ -146,7 +152,7 @@ fn main() {
 
     // Create kvm_run structure
     let vcpu_mmap_size: NonZeroUsize = match unsafe {
-        kvm_get_vcpu_mmap_size(kvm_fd.as_raw_fd())
+        kvm_get_vcpu_mmap_size(AsRawFd::as_raw_fd(&vm.sys_fd))
     } {
         Ok(result) => {
             NonZeroUsize::new(result as usize).expect("mmap_size is zero")
